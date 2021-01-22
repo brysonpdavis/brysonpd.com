9650d4e21a05c2c2d5d8d43960691001ab26d20e
Bryson <brysonpdavis@github.com>
2021-01-22 01:54:56 -0500

    fix grammatical errors

--- src/components/Introduction.svelte
+++ src/components/Introduction.svelte
@@ -54,7 +54,7 @@
     </p>
 
     <p>
-        in my life out&shy;side of soft&shy;ware develop&shy;ment, i find fulfill&shy;ment in many crea&shy;tive pur&shy;suits. music prod&shy;uction, photo&shy;graphy, graphic design are just a few of my many hobbies that keep me busy. 
-        both in my career and in my life, i most value crea&shy;tivity, clar&shy;ity, depend&shy;ability, and authen&shy;ticity, and i hope with every de&shy;cision i make to leave the world and the web a more beau&shy;tiful place.
+        in my life out&shy;side of soft&shy;ware develop&shy;ment, i find fulfill&shy;ment in many crea&shy;tive pur&shy;suits. music prod&shy;uction, photo&shy;graphy, and graphic design are just a few of my many hobbies that keep me busy. 
+        both in my career and in my personal life, i most value crea&shy;tivity, clar&shy;ity, depend&shy;ability, and authen&shy;ticity, and i hope with every de&shy;cision i make to leave the world and the web a more beau&shy;tiful place.
     </p>
 </div>
